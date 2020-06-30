a9375a1bee01dacb4b63e81113ffc7e9dda85a5a
kuldeep <kuldeep@technozer.com>
2020-06-30 13:51:26 +0530

    add swift file

---
+++ accuraocrSDK/AppConfrgation.swift
@@ -0,0 +1,16 @@
+//
+//  AppConfrgation.swift
+//  AccuraOCR
+//
+//  Created by Technozer on 29/06/20.
+//  Copyright © 2020 Technozer. All rights reserved.
+//
+
+import Foundation
+import AccuraOCR
+public class AccuraOCRSDK{
+    
+   public static func configur() -> Void{
+     FirebaseApp.configure()
+ }
+}
